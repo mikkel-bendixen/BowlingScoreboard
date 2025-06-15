2dcfb8f38f528247919fbdfb252038b6db8ee550
Mikkel Bendixen <MBE@kaastrupandersen.dk>
2025-06-15 22:59:55 +0200

    cleanedup game setup

--- src/Core/Application/BowlingGameChain.cs
+++ src/Core/Application/BowlingGameChain.cs
@@ -3,49 +3,18 @@ namespace Application;
 
 public class BowlingGameChain : IBowlingGame
 {
+    private const int TotalFrames = 10;
     public IReadOnlyCollection<IBowlingFrame> Frames => frames;
-    private List<IBowlingFrame> frames = new List<IBowlingFrame>();
-   
     public int Score => frames.Sum(frame => frame.Score ?? 0);
+
+    private List<IBowlingFrame> frames = [];
+
     private RollHandler rollHandler;
     public BowlingGameChain()
     {
-        var frame1Handler = new DefaultRollHandler();
-        var frame2Handler = new DefaultRollHandler();
-        var frame3Handler = new DefaultRollHandler();
-        var frame4Handler = new DefaultRollHandler();
-        var frame5Handler = new DefaultRollHandler();
-        var frame6Handler = new DefaultRollHandler();
-        var frame7Handler = new DefaultRollHandler();
-        var frame8Handler = new DefaultRollHandler();
-        var frame9Handler = new DefaultRollHandler();
-        var frame10Handler = new LastFrameRollHandler();
-
-        frame1Handler.SetNext(frame2Handler);
-        frame2Handler.SetNext(frame3Handler);
-        frame3Handler.SetNext(frame4Handler);
-        frame4Handler.SetNext(frame5Handler);
-        frame5Handler.SetNext(frame6Handler);
-        frame6Handler.SetNext(frame7Handler);
-        frame7Handler.SetNext(frame8Handler);
-        frame8Handler.SetNext(frame9Handler);
-        frame9Handler.SetNext(frame10Handler);
-
-        frames.Add(frame1Handler);
-        frames.Add(frame2Handler);
-        frames.Add(frame3Handler);
-        frames.Add(frame4Handler);
-        frames.Add(frame5Handler);
-        frames.Add(frame6Handler);
-        frames.Add(frame7Handler);
-        frames.Add(frame8Handler);
-        frames.Add(frame9Handler);
-        frames.Add(frame10Handler);
-
-        rollHandler = frame1Handler;
-        // You can add more handlers here if needed, e.g. for special frames or rules
-        // rollHandler.SetNext(new SpecialFrameHandler());
-
+        var chain = CreateChain().ToList();
+        frames = chain.Cast<IBowlingFrame>().ToList();
+        rollHandler = chain.First();
     }
 
     public void Roll(int pins)
@@ -53,12 +22,29 @@ public class BowlingGameChain : IBowlingGame
         rollHandler.Handle(pins);
     }
 
-
+    private IEnumerable<RollHandler> CreateChain()
+    {
+        RollHandler? lastHandler = null;
+        for (int i = 0; i < TotalFrames; i++)
+        {
+            RollHandler handler = i < TotalFrames - 1 ? new DefaultRollHandler() : new LastFrameRollHandler();
+            rollHandler ??= handler;
+            lastHandler?.SetNext(handler);
+            lastHandler = handler;
+            yield return handler;
+        }
+    }
 }
 
-internal abstract class RollHandler
+internal abstract class RollHandler : IBowlingFrame
 {
+    public abstract int? FirstRoll { get; protected set;  }
+
+    public abstract int? SecondRoll { get; protected set; }
+
+    public abstract int? Score { get; }
     protected RollHandler? Successor = null;
+
     public void SetNext(RollHandler successor)
     {
         Successor = successor;
@@ -66,16 +52,17 @@ internal abstract class RollHandler
     public abstract void Handle(int pins);
 }
 
-internal class LastFrameRollHandler : RollHandler, IBowlingFrame
+internal class LastFrameRollHandler : RollHandler
 {
+    public override int? FirstRoll { get; protected set; }
+    public override int? SecondRoll { get; protected set; }
+    public override int? Score => FirstRoll.GetValueOrDefault() + SecondRoll.GetValueOrDefault() + bonus1.GetValueOrDefault() + bonus2.GetValueOrDefault();
+
     private bool FrameCompleted => FirstRoll.HasValue && SecondRoll.HasValue || IsStrike;
     private bool IsStrike => FirstRoll.HasValue && FirstRoll == 10;
     private bool IsSpare => FirstRoll.HasValue && SecondRoll.HasValue && (FirstRoll + SecondRoll == 10);
-    public int? FirstRoll { get; private set; }
-    public int? SecondRoll { get; private set; }
-    public int? Score => FirstRoll.GetValueOrDefault() + SecondRoll.GetValueOrDefault() + Bonus1.GetValueOrDefault() + Bonus2.GetValueOrDefault();
-    public int? Bonus1 { get; private set; }
-    public int? Bonus2 { get; private set; }
+    private int? bonus1;
+    private int? bonus2;
 
     public override void Handle(int pins)
     {
@@ -83,13 +70,13 @@ internal class LastFrameRollHandler : RollHandler, IBowlingFrame
         {
             if (IsStrike)
             {
-                if (Bonus1 is null)
+                if (bonus1 is null)
                 {
-                    Bonus1 = pins;
+                    bonus1 = pins;
                 }
-                else if (Bonus2 is null)
+                else if (bonus2 is null)
                 {
-                    Bonus2 = pins;
+                    bonus2 = pins;
                 }
                 else
                 {
@@ -98,9 +85,9 @@ internal class LastFrameRollHandler : RollHandler, IBowlingFrame
             }
             else if (IsSpare)
             {
-                if (Bonus1 is null)
+                if (bonus1 is null)
                 {
-                    Bonus1 = pins;
+                    bonus1 = pins;
                 }
                 else
                 {
@@ -133,12 +120,13 @@ internal class LastFrameRollHandler : RollHandler, IBowlingFrame
 
 internal class DefaultRollHandler : RollHandler, IBowlingFrame
 {
+    public override int? FirstRoll { get; protected set; }
+    public override int? SecondRoll { get; protected set; }
+    public override int? Score => FirstRoll.GetValueOrDefault() + SecondRoll.GetValueOrDefault() + Bonus1.GetValueOrDefault() + Bonus2.GetValueOrDefault();
     private bool FrameCompleted => FirstRoll.HasValue && SecondRoll.HasValue || IsStrike;
     private bool IsStrike => FirstRoll.HasValue && FirstRoll == 10;
     private bool IsSpare => FirstRoll.HasValue && SecondRoll.HasValue && (FirstRoll + SecondRoll == 10);
-    public int? FirstRoll { get; private set; }
-    public int? SecondRoll { get; private set; }
-    public int? Score => FirstRoll.GetValueOrDefault() + SecondRoll.GetValueOrDefault() + Bonus1.GetValueOrDefault() + Bonus2.GetValueOrDefault();
+
     public int? Bonus1 { get; private set; }
     public int? Bonus2 { get; private set; }
 
